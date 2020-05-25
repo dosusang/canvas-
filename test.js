50acbb3cf605a2d5fa3482dcf746f1864889eb1c
dosusang <2501488984@qq.com>
2020-05-25 21:03:14 +0800

    fitst

---
+++ test.js
@@ -0,0 +1,64 @@
+class ball {
+  constructor(x, y) {
+    this.sx = Math.random()*10-5
+    this.sy = 0
+    this.x = x
+    this.y = y
+    this.r = Math.random()*20+10
+    this.color = colorArray[x%5]
+  }
+  
+  move() {
+    this.sy+=0.1
+    if(this.sx > 0) {
+      this.sx -= 0.005
+    } else {
+      this.sx += 0.005
+    }
+   
+    if (this.x + this.sx > w - this.r || this.x + this.sx < 0) {
+      this.sx = -0.5*this.sx
+    }
+    if (this.y + this.sy > h - this.r || this.y + this.sy < 0) {
+      this.sy = -0.7*this.sy
+    }
+    this.x += this.sx
+    this.y += this.sy
+  }
+
+  drawself() {
+    this.move()
+    ctx.fillStyle = this.color
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false)
+    ctx.fill()
+  }
+}
+
+/** @type {HTMLCanvasElement} */
+var c = document.getElementById("testcanvas");
+var ctx = c.getContext("2d");
+var balls = []
+var w,h
+var colorArray = ["rgb(65, 105, 225)", "rgb(65, 166, 225)", "rgb(65, 214, 225)", "rgb(65, 225, 190)", "rgb(65, 225, 132)"];
+function draw() {
+  ctx.clearRect(0, 0, w, h);
+  for(let i = 0; i < balls.length; i++) {
+    balls[i].drawself()
+  }
+  requestAnimationFrame(draw);
+}
+function getWindow_size() {
+  w = window.innerWidth;
+  h = window.innerHeight;
+  c.width = w;
+  c.height = h;
+}
+window.onload = function () { 
+  getWindow_size()
+  document.addEventListener("mousedown", function (event) {
+      balls.push(new ball(event.x, event.y));
+  });
+}
+
+draw()
